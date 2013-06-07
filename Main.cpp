4d07f0a2e514df16310b39c8f70e9d6b58ada3af
unknown <lanyab@Coolidge.vrac.iastate.edu>
2013-06-07 10:51:21 -0500

    Fixed the ASCII art

--- Main.cpp
+++ Main.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 int main(){
 	int control = 0;
-	bool lose = false;
-	bool win = false;
+	bool lose = true;
+	bool win = true;
 	clock_t start;
 	double duration;
 	
 	while (1){
 		// pretty ASCII art that will print every time you loop back to the main menu
-		cout << "          \|/ " << endl;
+		cout << "          \\|/ " << endl;
 		cout << "         .-*-   " << endl;
-		cout << "        / /|\    " << endl;
+		cout << "        / /|\\    " << endl;
 		cout << "       _L_ " << endl;
-		cout << "     ,"   ".		_ " << endl;                                            
-		cout << " (\ /  O O  \ /)  | |_____     _     _____                           " << endl;  
-		cout << "  \|    _    |/   |_|   __|___| |___|   __|_ _ _ ___ ___ ___ ___ ___ " << endl;
-		cout << "    \  (_)  /       |__   | . | | -_|__   | | | | -_| -_| . | -_|  _| " << endl;
-		cout << "    _/.___,\_       |_____|___|_|___|_____|_____|___|___|  _|___|_|  " << endl;
-		cout << "   (_/     \_)                                          |_|       " << endl;
+		cout << "     ,\"   \".        _ " << endl;                                            
+		cout << " (\\ /  O O  \\ /)   | |______    __    ______                           " << endl;  
+		cout << "  \\|    _    |/    |_|   __|___| |___|   __|_ _ _ ___ ___ ___ ___ ___ " << endl;
+		cout << "    \\  (_)  /        |__   | . | | -_|__   | | | | -_| -_| . | -_|  _| " << endl;
+		cout << "    _/.___,\\_        |_____|___|_|___|_____|_____|___|___|  _|___|_|  " << endl;
+		cout << "   (_/     \\_)                                           |_|       " << endl;
 
 		// MENU OPTIONS
 		cout << "1 - Start Game			2 - Credits			3 - Quit " << endl;
@@ -35,7 +35,7 @@ int main(){
 			start = clock();
 			// GAME CODE GOES HERE
 
-			if (lose == true) {
+			if (lose) {
 				/*
 				cout << " __   __   ___    _   _             _       ___     ___    _____  " << endl;
 				cout << " \ \ / /  / _ \  | | | |    o O O  | |     / _ \   / __|  |_   _| " << endl;
@@ -45,19 +45,19 @@ int main(){
 				cout << " '-0-0-'''-0-0-'''-0-0-'./o--000'''-0-0-''`-0-0-'''-0-0-'''-0-0-' " << endl;
 				*/
 				cout << "__     ______  _    _   _      ____   _____ _______ " << endl;
-				cout << "\ \   / / __ \| |  | | | |    / __ \ / ____|__   __| " << endl;
-				cout << " \ \_/ / |  | | |  | | | |   | |  | | (___    | |   " << endl;
-				cout << "  \   /| |  | | |  | | | |   | |  | |\___ \   | |   " << endl;
+				cout << "\\ \\   / / __ \\| |  | | | |    / __ \\ / ____|__   __| " << endl;
+				cout << " \\ \\_/ / |  | | |  | | | |   | |  | | (___    | |   " << endl;
+				cout << "  \\   /| |  | | |  | | | |   | |  | |\\___ \\   | |   " << endl;
 				cout << "   | | | |__| | |__| | | |___| |__| |____) |  | |   " << endl;
-				cout << "   |_|  \____/ \____/  |______\____/|_____/   |_|   " << endl;
+				cout << "   |_|  \\____/ \\____/  |______\\____/|_____/   |_|   " << endl;
 			}
-			if (win == true){
+			if (win){
 				cout << " __     ______  _    _  __          _______ _   _ " << endl;
-				cout << " \ \   / / __ \| |  | | \ \        / /_   _| \ | |" << endl;
-				cout << "  \ \_/ / |  | | |  | |  \ \  /\  / /  | | |  \| |" << endl;
-				cout << "   \   /| |  | | |  | |   \ \/  \/ /   | | | . ` |" << endl;
-				cout << "    | | | |__| | |__| |    \  /\  /   _| |_| |\  |" << endl;
-				cout << "    |_|  \____/ \____/      \/  \/   |_____|_| \_|" << endl;
+				cout << " \\ \\   / / __ \\| |  | | \\ \\        / /_   _| \\ | |" << endl;
+				cout << "  \\ \\_/ / |  | | |  | |  \\ \\  /\\  / /  | | |  \\| |" << endl;
+				cout << "   \\   /| |  | | |  | |   \\ \\/  \\/ /   | | | . ` |" << endl;
+				cout << "    | | | |__| | |__| |    \\  /\\  /   _| |_| |\\  |" << endl;
+				cout << "    |_|  \\____/ \\____/      \\/  \\/   |_____|_| \\_|" << endl;
 			}
 
 			duration = (clock() - start ) / (double)CLOCKS_PER_SEC;
