8b0f128828b5a15fb8962ea969a704bae20111fc
Craig Disselkoen <cdissel@iastate.edu>
2013-06-07 16:23:39 -0500

    some unimportant changes

--- Cell.hpp
+++ Cell.hpp
@@ -18,7 +18,7 @@ public:
 	Cell();
 	void add_adjacent_cell(Cell* adj); // argument is a Cell pointer to be added to the list of pointers to adjacent Cells.
 	void flagged(void); // call this to mark this cell as flagged (or to unflag, if this cell is already flagged). Works properly regardless of whether this cell is currently UNCLICKED, CLICKED (in which case no effect), or FLAGGED.
-	virtual bool clicked(void) = 0;  // call this when this cell is clicked
+	virtual bool clicked(void) = 0;  // call this when this cell is clicked. Returns false if the cell was a mine, true if the cell was 'safe'.
 	virtual void print(void) = 0;  // call this to print the cell (as part of the 'SoleSweeper grid)
 
 protected:
